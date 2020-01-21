bb1406797dde8093c7ea787015820d46b4dbe2a3
Zhai Shuo <38308786+github-zs@users.noreply.github.com>
2020-01-21 16:48:25 +0800

    fix: fix code

--- learn/practice/magic.py
+++ learn/practice/magic.py
@@ -1,12 +1,15 @@
 '''
 找出两个两位数，分别十位个位互换后，乘积不变
 '''
-
+import time
+start = time.time()
 for i in range(10, 100):
 	for j in range(10, 100):
 		ione = i % 10
 		iten = i // 10
 		jone = j % 10
-		jten = j //10
+		jten = j // 10
 		if(i * j == (ione * 10 + iten) * (jone * 10 + jten)):
 			print('i={},j={}'.format(i, j))
+
+print("一共用时: {}s".format(time.time() - start))
