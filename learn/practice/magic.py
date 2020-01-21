de41a580a8ed590b450c9ab064448909e5d1bea0
Zhai Shuo <38308786+github-zs@users.noreply.github.com>
2020-01-21 15:53:14 +0800

    add: magic double-digit

---
+++ learn/practice/magic.py
@@ -0,0 +1,12 @@
+'''
+找出两个两位数，分别十位个位互换后，乘积不变
+'''
+
+for i in range(10, 100):
+	for j in range(10, 100):
+		ione = i % 10
+		iten = i // 10
+		jone = j % 10
+		jten = j //10
+		if(i * j == (ione * 10 + iten) * (jone * 10 + jten)):
+			print('i={},j={}'.format(i, j))
